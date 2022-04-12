257173080eff2e78b0e814564cafb368a1863cbf
aitor-arronte <aitoraalvarez@gmail.com>
2022-04-11 23:34:58 -1000

    added dataset

--- data/dataset.py
+++ data/dataset.py
@@ -16,6 +16,6 @@ def normalize_text(sentences):
 	normalized_sentences =[]
 	for txt in sentences:
 		tkns = word_tokenizer.tokenize(txt)
-		tkns = [''.join(t.split('-')).lower() for t in tkns if t not in stop_words and t not in '@.,!#$%*:;"']
+		tkns = [''.join(t.split('-')).lower() for t in tkns if t not in stop_words and t not in '@.,!#$%*:;"' and 'http' not in t and 'www' not in t]
 		normalized_sentences.append(' '.join(tkns))
 	return normalized_sentences
