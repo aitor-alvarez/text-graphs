8a514aeab3909b417982dfe2e4d795c96ce8ffc2
aitor-arronte <aitoraalvarez@gmail.com>
2022-04-13 16:40:09 -1000

    added bert embeddings

--- utils/preprocess.py
+++ utils/preprocess.py
@@ -4,8 +4,9 @@ import os
 import pandas as pd
 import json
 from gensim.models import Word2Vec
-from sentence_transformers import SentenceTransformer, util
+from sentence_transformers import SentenceTransformer
 from transformers import AutoModel, AutoTokenizer
+import torch
 
 
 def create_tree_pheme(dir):
@@ -42,12 +43,34 @@ def word_embeddings(tweets):
 #Bertweet embedding
 def bert_tweet(tweets, fine_tune=False):
 	tokenizer = AutoTokenizer.from_pretrained("vinai/bertweet-base", use_fast=False)
+	tweet_model = AutoModel.from_pretrained("vinai/bertweet-base")
+	tokens = {'input_ids': [], 'attention_mask': []}
 	if fine_tune:
+		pass
+	else:
+		for sen in tweets:
+			tkn = tokenizer.encode_plus(sen, max_length=130,
+			                            truncation=True, padding='max_length',
+			                            return_tensors='pt')
+			tokens['input_ids'].append(tkn['input_ids'][0])
+			tokens['attention_mask'].append(tkn['attention_mask'][0])
 
+		tokens['input_ids'] = torch.stack(tokens['input_ids'])
+		tokens['attention_mask'] = torch.stack(tokens['attention_mask'])
+
+		with torch.no_grad():
+			output = tweet_model(**tokens)
+
+		tweet_embeddings = mean_pooling(output, tokens['attention_mask'])
 
 
 def transformer_sentences(tweets, fine_tune=False):
+	model = SentenceTransformer('bert-base-nli-mean-tokens')
 	if fine_tune:
+		pass
+	else:
+		embeddings = model.encode(tweets)
+	return embeddings
 
 
 
@@ -60,3 +83,10 @@ def normalize_text(sentences):
 		tkns = [''.join(t.split('-')).lower() for t in tkns if t not in stop_words and t not in '@.,!#$%*:;"' and 'http' not in t and 'www' not in t]
 		normalized_sentences.append(' '.join(tkns))
 	return normalized_sentences
+
+
+#Mean Pooling using the attention mask of the tokens
+def mean_pooling(model_output, attention_mask):
+    token_embeddings = model_output[0]
+    input_mask_expanded = attention_mask.unsqueeze(-1).expand(token_embeddings.size()).float()
+    return torch.sum(token_embeddings * input_mask_expanded, 1) / torch.clamp(input_mask_expanded.sum(1), min=1e-9)
